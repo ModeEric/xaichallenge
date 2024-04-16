1f3ab76bac78659a8c979ebcd012bbf518b65e4c
Eric Modesitt <ericmodesitt@Erics-MacBook-Pro-6.local>
2024-04-16 13:10:16 -0500

    1.3 Rust The Book

---
+++ rust/projects/hello_cargo/target/debug/deps/hello_cargo-ed6fb0b8e89ef810.d
@@ -0,0 +1,5 @@
+/Users/ericmodesitt/Desktop/30days/rust/projects/hello_cargo/target/debug/deps/hello_cargo-ed6fb0b8e89ef810: src/main.rs
+
+/Users/ericmodesitt/Desktop/30days/rust/projects/hello_cargo/target/debug/deps/hello_cargo-ed6fb0b8e89ef810.d: src/main.rs
+
+src/main.rs:
---
+++ rust/projects/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/ericmodesitt/Desktop/30days/rust/projects/hello_cargo/target/debug/hello_cargo: /Users/ericmodesitt/Desktop/30days/rust/projects/hello_cargo/src/main.rs
